6ffada16f555f88d82ee4057fc1b41d5a12ee5de
Harash <harash.poriya@gmail.com>
2024-12-05 14:30:21 +0530

    Frontend client side

---
+++ src/app/(client)/Options/Svgdown.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./Option.css"
+ const Svgdown = () => {
+  return (
+    <div> <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="dropdown-icon"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <path d="m6 9 6 6 6-6"/>
+  </svg></div>
+  )
+}
+export default Svgdown;
