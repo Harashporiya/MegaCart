5d88cacb09792a552b44e136546ed54036eff7f4
Harash <Harashporiya>
2024-12-05 14:31:50 +0530

    Frontend client side

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Home from './home/home'
+import Header from './(client)/Header/Header'
+import Options from './(client)/Options/Option'
+
+const page = () => {
+  return (
+    <>
+    <Home/>
+    {/* <Header/>
+    <Options/> */}
+    </>
+  )
+}
+
+export default page
