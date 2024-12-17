8afbfcd017d481331623d4ab3655b92f2358f593
Harash <Harashporiya>
2024-12-17 09:51:38 +0530

    install shadcn/ui

---
+++ src/app/components/app-sidebars.tsx
@@ -0,0 +1,314 @@
+import * as React from "react"
+import { GalleryVerticalEnd } from "lucide-react"
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
+  SidebarRail,
+} from "@/components/ui/sidebar"
+
+// This is sample data.
+const data = {
+  navMain: [
+    {
+      title: "Groceries",
+      url: "#",
+      items: [
+        {
+          title: "Fresh vegetables",
+          url: "#",
+        },
+        {
+          title: "Fresh fruits",
+          url: "#",
+        },
+        {
+            title: "Canned foods",
+            url: "#",
+          },
+          {
+            title: "Sauces",
+            url: "#",
+          },
+          {
+            title: "Oils & Vinegars",
+            url: "#",
+          },
+      ],
+    },
+    {
+      title: "Premium Fruits",
+      url: "#",
+      items: [
+        {
+          title: "Imported Plum",
+          url: "#",
+        },
+        {
+          title:"Dragon fruit",
+          url: "#",
+          isActive: true,
+        },
+        {
+          title: "Apple",
+          url: "#",
+        },
+        {
+          title: "Longan - imported",
+          url: "#",
+        },
+        {
+          title: "Green Kiwi",
+          url: "#",
+        },
+        {
+          title: "Apple - Pink Lady",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Home & Kitchen",
+      url: "#",
+      items: [
+        {
+          title: "Cookware",
+          url: "#",
+        },
+        {
+          title: "Kitchen Appliances",
+          url: "#",
+        },
+        {
+          title: "Dining & Serveware",
+          url: "#",
+        },
+        {
+          title: "Home Decor",
+          url: "#",
+        },
+        {
+          title: "Cleaning Supplies",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Fashion",
+      url: "#",
+      items: [
+        {
+          title: "Women's Clothing",
+          url: "#",
+        },
+        {
+          title: "Men's Clothing",
+          url: "#",
+        },
+        {
+          title: "Kids' Clothing",
+          url: "#",
+        },
+        {
+          title: "Accessories",
+          url: "#",
+        },
+        {
+          title: "Footwear",
+          url: "#",
+        },
+        {
+            title: "Bags & Luggage",
+            url: "#",
+          },
+      ],
+    },
+    {
+      title: "Electronic",
+      url: "#",
+      items: [
+        {
+          title: "Smartphones ",
+          url: "#",
+        },
+        {
+            title: "Laptops",
+            url: "#",
+          },
+          {
+            title: "Tablets",
+            url: "#",
+          },
+          {
+            title: "Headphones",
+            url: "#",
+          },
+          {
+            title: "Smart Watches",
+            url: "#",
+          },
+          {
+            title: "Speakers",
+            url: "#",
+          },
+          {
+            title: "Printers",
+            url: "#",
+          },
+          {
+            title: "GameConsoles",
+            url: "#",
+          },
+      ],
+    },
+    {
+        title: "Beauty",
+        url: "#",
+        items: [
+          {
+            title: "Skincare",
+            url: "#",
+          },
+          {
+            title: "Makeup",
+            url: "#",
+          },
+          {
+            title: "Hair Care",
+            url: "#",
+          },
+          {
+            title: "Fragrances",
+            url: "#",
+          },
+          {
+            title: "Personal Care",
+            url: "#",
+          },
+          {
+              title: "Bath & Body",
+              url: "#",
+            },
+        ],
+      },
+      {
+        title: "Home Improvement",
+        url: "#",
+        items: [
+          {
+            title: "Tools",
+            url: "#",
+          },
+          {
+            title: "Lighting",
+            url: "#",
+          },
+          {
+            title: "Hardware",
+            url: "#",
+          },
+          {
+            title: "Paint & Supplies",
+            url: "#",
+          },
+          {
+            title: "Bathroom Fixtures",
+            url: "#",
+          },
+          {
+              title: "Heating & Cooling",
+              url: "#",
+            },
+        ],
+      },
+      {
+        title: "Sports, Toys & Luggage",
+        url: "#",
+        items: [
+          {
+            title: "Outdoor Sports",
+            url: "#",
+          },
+          {
+            title: "Indoor Games",
+            url: "#",
+          },
+          {
+            title: "Fitness Equipment",
+            url: "#",
+          },
+          {
+            title: "Toys & Collectibles",
+            url: "#",
+          },
+          {
+            title: "Luggage",
+            url: "#",
+          },
+          {
+              title: "Travel Accessories",
+              url: "#",
+            },
+        ],
+      },
+  ],
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+              <a href="#">
+                <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+                  <GalleryVerticalEnd className="size-4" />
+                </div>
+                <div className="flex flex-col gap-0.5 leading-none">
+                  <span className="font-semibold">Documentation</span>
+                  <span className="">v1.0.0</span>
+                </div>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarMenu>
+            {data.navMain.map((item) => (
+              <SidebarMenuItem key={item.title}>
+                <SidebarMenuButton asChild>
+                  <a href={item.url} className="font-medium">
+                    {item.title}
+                  </a>
+                </SidebarMenuButton>
+                {item.items?.length ? (
+                  <SidebarMenuSub>
+                    {item.items.map((item) => (
+                      <SidebarMenuSubItem key={item.title}>
+                        <SidebarMenuSubButton asChild isActive={item.isActive}>
+                          <a href={item.url}>{item.title}</a>
+                        </SidebarMenuSubButton>
+                      </SidebarMenuSubItem>
+                    ))}
+                  </SidebarMenuSub>
+                ) : null}
+              </SidebarMenuItem>
+            ))}
+          </SidebarMenu>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarRail />
+    </Sidebar>
+  )
+}
